3a3a311178efbee277a4e659671db5005f7cc0e7
Pin-Xuan She <snakeneedy@gmail.com>
2020-09-03 13:14:48 +0800

    🔨 gulpfile.js build code and refresh browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+import browserSync from 'browser-sync';
 import gulp from 'gulp';
 import gulpAutoprefixer from 'gulp-autoprefixer';
 import gulpCleanCss from 'gulp-clean-css';
@@ -20,3 +21,22 @@ gulp.task('build:js', () => gulp.src('src/**/*.js')
   .pipe(gulp.dest('build/')));
 
 gulp.task('build', gulp.parallel(['build:html', 'build:css', 'build:js']));
+
+gulp.task('watch', gulp.series([
+  'build',
+  () => {
+    gulp.watch('src/**/*.pug', gulp.series('build:html'));
+    gulp.watch('src/**/*.scss', gulp.series('build:css'));
+    gulp.watch('src/**/*.js', gulp.series('build:js'));
+
+    browserSync.init({
+      server: {
+        baseDir: 'build/',
+      },
+    });
+    gulp.watch('build/', (done) => {
+      browserSync.reload();
+      done();
+    });
+  },
+]));
