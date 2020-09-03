d5d5eab56dc8ea17ede6417621bf9fe650da4aa2
Pin-Xuan She <snakeneedy@gmail.com>
2020-09-03 12:53:34 +0800

    🔨 Update gulpfile.js: compress js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import gulpAutoprefixer from 'gulp-autoprefixer';
 import gulpCleanCss from 'gulp-clean-css';
 import gulpPug from 'gulp-pug';
 import gulpSass from 'gulp-sass';
+import gulpTerser from 'gulp-terser';
 
 gulp.task('build:html', () => gulp.src('src/**/*.pug')
   .pipe(gulpPug())
@@ -12,4 +13,8 @@ gulp.task('build:css', () => gulp.src('src/**/*.scss')
   .pipe(gulpSass())
   .pipe(gulpAutoprefixer({ cascade: true }))
   .pipe(gulpCleanCss())
+  .pipe(gulp.dest('build/')));
+
+gulp.task('build:js', () => gulp.src('src/**/*.js')
+  .pipe(gulpTerser())
   .pipe(gulp.dest('build/')));
