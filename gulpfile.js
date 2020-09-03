1350856bee5614e354936e725ee539dd034e3e00
Pin-Xuan She <snakeneedy@gmail.com>
2020-09-03 12:44:39 +0800

    🔨 Update gulpfile.js: build css from sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 import gulp from 'gulp';
+import gulpAutoprefixer from 'gulp-autoprefixer';
+import gulpCleanCss from 'gulp-clean-css';
 import gulpPug from 'gulp-pug';
+import gulpSass from 'gulp-sass';
 
 gulp.task('build:html', () => gulp.src('src/**/*.pug')
   .pipe(gulpPug())
+  .pipe(gulp.dest('build/')));
+
+gulp.task('build:css', () => gulp.src('src/**/*.scss')
+  .pipe(gulpSass())
+  .pipe(gulpAutoprefixer({ cascade: true }))
+  .pipe(gulpCleanCss())
   .pipe(gulp.dest('build/')));
